649c98a3052911b98f59e3352bd1037c65f7ea70
Minh Anh Bui <minhanhbui@MacBook-Air-Ellie.local>
2024-03-04 01:20:56 -0500

    Add authentication

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,7 +1,29 @@
 <script setup>
+import { ref } from 'vue';
+import { useAuthStore } from '@/Stores/AuthStore';
+import { useRouter } from 'vue-router';
 
+const email = ref('');
+const password = ref('');
+
+const router = useRouter();
+const authStore = useAuthStore();
+const { login, error } = authStore;
+
+async function submit() {
+  await login({ email: email.value, password: password.value });
+  if (!error) {
+    router.push({ name: 'ticket-queue' });
+  }
+}
 </script>
 
 <template>
-<h1>This is Login page</h1>
+  <div>
+    <form @submit.prevent="submit">
+      <input type="text" v-model="email" />
+      <input type="text" v-model="password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
